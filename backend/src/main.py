1a83b4806715db6fc3b044f9daf19eeade3dbd7a
Joseph <munemojoseph332@gmail.com>
2025-07-07 22:19:37 +0200

    updated main.py again

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,13 @@
+from ast import Raise
 import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
-MY_PROJECT = os.getenv("MY_PROJECT") or "This is my project"
-API_KEY = os.getenv("API_KEY")
+MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
+API_KEY = os.environ.get("API_KEY")
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' was not set")
 
 @app.get("/")
 def read_index():
