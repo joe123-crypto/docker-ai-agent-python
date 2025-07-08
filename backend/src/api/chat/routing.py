975b3f2476c3614331a3b59f0a11884f78f24a5a
Joseph <munemojoseph332@gmail.com>
2025-07-08 21:40:29 +0200

    added an agent

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from api.db import get_session
+from api.ai.schemas import EmailMessageSchema
+from api.ai.services import generate_email_messages
 from .models import ChatMessagePayload,ChatMessage,ChatMessageListItem
 
 router = APIRouter()
@@ -12,21 +14,25 @@ def chat_health():
     return {"status":"Ok"}
 
 #curl -Uri "http://localhost:8080/api/chats/recent/" -Method GET -ContentType "application/json"
+#curl -Uri "https://docker-ai-agent-python-production-81b8.up.railway.app/api/chats/recent/"
+
 @router.get("/recent/",response_model=List[ChatMessageListItem])
 def chat_list_messages(session: Session = Depends(get_session)):
     query = select(ChatMessage)
     results = session.exec(query).fetchall()[:10]
     return results
-#curl -Uri "http://localhost:8080/api/chats/" -Method POST -Body '{"message":"hello world"}' -ContentType "application/json"
-@router.post("/",response_model=ChatMessage)
+#curl -Uri "http://localhost:8080/api/chats/" -Method POST -Body '{"message":"give me a summary of why its good to go outside"}' -ContentType "application/json"
+
+#curl -Uri "https://docker-ai-agent-python-production-81b8.up.railway.app/api/chats/" -Method POST -Body '{"message":"hello world"}' -ContentType "application/json"
+@router.post("/",response_model=EmailMessageSchema)
 def chat_create_message(
     payload: ChatMessagePayload,
     session: Session = Depends(get_session)
 ):
     data = payload.model_dump()
-    print(data)
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    session.refresh(obj)
-    return obj
+    #session.refresh(obj)
+    response = generate_email_messages(payload.message)
+    return response
---
+++ docker-commands.md
@@ -0,0 +1,57 @@
+
+#Docker model runner
+
+_From host terminal_
+```bash
+curl -Uri "http://localhost:12434/engines/llama.cpp/v1/chat/completions" `
+    -Method POST `
+    -ContentType "application/json" `
+    -Body '{
+        "model": "ai/smollm2",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Please write 500 words about the fall of Rome."
+            }
+        ]
+    }'
+```
+_From within a container_
+```bash
+curl -Uri "http://model-runner.docker.internal/engines/llama.cpp/v1/chat/completions" `
+    -Method POST `
+    -ContentType "application/json" `
+    -Body '{
+        "model": "ai/smollm2",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Please write 500 words about the fall of Rome."
+            }
+        ]
+    }'
+    
+curl http://model-runner.docker.internal/engines/llama.cpp/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -d '{
+        "model": "ai/smollm2",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Please write 500 words about the fall of Rome."
+            }
+        ]
+    }'
+```
